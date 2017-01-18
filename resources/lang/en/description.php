b4532ace2ba09989850d73b09b5ed8d4c0350aaf
Heppy <tehheppy@gmail.com>
2017-01-18 21:41:44 +0100

    Small description fix

--- resources/lang/en/description.php
+++ resources/lang/en/description.php
@@ -2,7 +2,7 @@
 
 return [
     'set' => [
-        \App\Enum\SetType::MONSTER => ':name monster set, helm found in :dungeon and shoulders in :pledgeChest chest',
+        \App\Enum\SetType::MONSTER => ':name monster set, helm found in :dungeon (veteran) and shoulders in :pledgeChest chest',
         \App\Enum\SetType::CRAFTED => ':name set craftable in :zones and needs :traits traits researched',
         \App\Enum\SetType::DUNGEON => ':name set found in :dungeons',
         \App\Enum\SetType::ZONE => ':name set found in :zones',
