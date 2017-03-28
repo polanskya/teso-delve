951e644eb8ee12b3b7bc328d256a03fbbc7d82c0
Heppy <tehheppy@gmail.com>
2017-03-28 23:09:37 +0200

    Quick and easy admin stats
    Quick and easy statistics for admin dashboard.

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,15 +1,36 @@
 <?php namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Model\Character;
+use App\Model\ItemSale;
 use App\Model\Role\Permission;
 use App\Model\Role\Role;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    const SEEN_AT_DAYS = 1;
+
 
     public function index() {
-        return view('admin.index');
+        $seenAt = Carbon::now()->subDays(self::SEEN_AT_DAYS);
+
+        $data = [
+            'total_users' => User::count(),
+            'new_users' => User::where('created_at', '>=', $seenAt)->count(),
+            'users_activity' => User::where('seen_at', '>=', $seenAt)->count(),
+            'dumps_activity' => User::where('dumpUploaded_at', '>=', $seenAt)->count(),
+
+            'total_characters' => Character::count(),
+            'new_characters' => Character::where('created_at', '>=', $seenAt)->count(),
+
+            'total_sales' => ItemSale::count(),
+            'sales_activity' => ItemSale::where('created_at', '>=', $seenAt)->count(),
+        ];
+
+        return view('admin.index', compact('data'));
     }
 
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,4 +2,63 @@
 
 @section('content')
 
+    <div class="row m-t-1">
+
+        <!-- START Revenue -->
+        <div class="col-lg-3 col-md-4">
+            <div class="panel panel-default b-a-0 bg-primary-i">
+                <div class="panel-heading b-b-0">Users <span class="label label-white label-outline pull-right">{{$data['total_users']}}</span></div>
+                <div class="panel-body text-center p-t-0">
+                    <div class="col-md-6">
+                        <h1 class="m-t-0 m-b-0 f-w-300">{{$data['new_users']}}</h1>
+                        <p class="text-white">New Users</p>
+                    </div>
+                    <div class="col-md-6">
+                        <h1 class="m-t-0 m-b-0 f-w-300">{{$data['users_activity']}}</h1>
+                        <p class="text-white">Seen last 24 hrs</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END Revenue -->
+
+        <!-- START Total Item Sold -->
+        <div class="col-lg-3 col-md-4">
+            <div class="panel panel-default b-a-0 bg-gray-dark bg-success-i">
+                <div class="panel-heading b-b-0">Characters <span class="label label-white label-outline  pull-right">{{$data['total_characters']}}</span></div>
+                <div class="panel-body text-center p-t-0">
+                    <h1 class="m-t-0 m-b-0 f-w-300">{{$data['new_characters']}}</h1>
+                    <p class="text-white">New Characters</p>
+                </div>
+            </div>
+        </div>
+        <!-- END Total Item Sold -->
+
+        <!-- START Total Visitor -->
+        <div class="col-lg-3 col-md-4">
+            <div class="panel panel-default b-a-0 bg-gray-dark bg-warning-i">
+                <div class="panel-heading b-b-0">Uploads</div>
+                <div class="panel-body text-center p-t-0">
+                    <h1 class="m-t-0 m-b-0 f-w-300">{{$data['dumps_activity']}}</h1>
+                    <p class="text-white">Last 24 hrs</p>
+                </div>
+            </div>
+        </div>
+        <!-- END Total Visitor -->
+
+        <!-- START Total Visitor -->
+        <div class="col-lg-3 col-md-4">
+            <div class="panel panel-default b-a-0 bg-gray-dark bg-danger-i">
+                <div class="panel-heading b-b-0">Sales <span class="label label-white label-outline  pull-right">{{$data['total_sales']}}</span></div>
+                <div class="panel-body text-center p-t-0">
+                    <h1 class="m-t-0 m-b-0 f-w-300">{{$data['sales_activity']}}</h1>
+                    <p class="text-white">Sales last 24 hrs</p>
+                </div>
+            </div>
+
+        </div>
+        <!-- END Total Visitor -->
+
+    </div>
+
 @endsection
