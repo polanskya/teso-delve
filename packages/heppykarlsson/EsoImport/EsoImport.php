e21fe25d38fcbdd16a76154aa70d03153f09ef10
Heppy <tehheppy@gmail.com>
2017-05-09 23:04:07 +0200

    HOTFIX: Added import memory env.

--- packages/heppykarlsson/EsoImport/EsoImport.php
+++ packages/heppykarlsson/EsoImport/EsoImport.php
@@ -25,7 +25,7 @@ class EsoImport
 
     public function import($file_path) {
         set_time_limit(120);
-        ini_set('memory_limit','24M');
+        ini_set('memory_limit', env('IMPORT_MAX_MEMORY', 24).'M');
 
         $updateStart = Carbon::now()->subSecond(5);
 
