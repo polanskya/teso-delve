3b449f94cfdc352926abf45590a041143f23d3d9
Alex Polansky <alex.polansky@gmail.com>
2020-02-06 13:41:23 -0500

    redis and more fixex

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,17 +1,17 @@
 
     <div class="form-group">
         <div class="col-sm-12">
-            <input type="text" class="form-control" name="role[name]" placeholder="Name" value="{{$role->name or ''}}">
+            <input type="text" class="form-control" name="role[name]" placeholder="Name" value="{{empty($role->name) ? '' : $role->name}}">
         </div>
     </div>
     <div class="form-group">
         <div class="col-sm-12">
-            <input type="text" class="form-control" name="role[display_name]" placeholder="Display name" value="{{$role->display_name or ''}}">
+            <input type="text" class="form-control" name="role[display_name]" placeholder="Display name" value="{{empty($role->display_name) ? '' : $role->display_name}}">
         </div>
     </div>
     <div class="form-group">
         <div class="col-sm-12">
-            <textarea class="form-control" name="role[description]" placeholder="Description" rows="6">{{$role->description or ''}}</textarea>
+            <textarea class="form-control" name="role[description]" placeholder="Description" rows="6">{{empty($role->description) ? '' : $role->description}}</textarea>
         </div>
     </div>
 
